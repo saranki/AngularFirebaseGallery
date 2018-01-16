0b83d73ac8b699029b354270248ca5861da9b462
Saranki M <m.saranki@gmail.com>
2018-01-16 12:53:25 +0530

    Completed Image detail components

--- src/app/image-details/image-details.component.ts
+++ src/app/image-details/image-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../services/image.service';
+import { ActivatedRoute } from '@angular/router';
+import { GalleryImage } from '../models/galleryImage.models';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-image-details',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageDetailsComponent implements OnInit {
 
-  constructor() { }
+  private imageUrl = '';
 
+  //Contains the information about a route associated with a component loaded in an outlet.
+  //An ActivatedRoute can also be used to traverse the router state tree
+  constructor(private imageService: ImageService, private router: ActivatedRoute) { }
+
+  //retuens a promise
+  getImageUrl(key: string) {
+    this.imageService.getImage(key)
+    //.then(imageUrl => this.imageUrl = imageUrl.url);
+    .then(image=> (console.log(image.url)));
+  }
+  //Once the page loaded the page's url should be the selected image's url from firebase 
   ngOnInit() {
+    this.getImageUrl(this.router.snapshot.params['id']);
   }
 
 }
